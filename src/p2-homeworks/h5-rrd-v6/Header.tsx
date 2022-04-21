64cec541cb0278dd0f21c0432498b43656de9ef2
isxodus <isxodus@gmail.com>
2022-04-21 17:57:43 +0300

    hw5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import css from "./Header.module.css"
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={css.mySidenav}>
+            <span>
+                <NavLink className={css.menu} to={PATH.PRE_JUNIOR}>pre_junior</NavLink>
+                <NavLink className={css.menu} to={PATH.JUNIOR}>junior</NavLink>
+                <NavLink className={css.menu} to={PATH.JUNIOR_PLUS}>junior+</NavLink>
+            </span>
         </div>
     )
 }
