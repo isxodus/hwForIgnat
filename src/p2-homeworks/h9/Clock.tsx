03229643f13f2aa89ee2f82eca70b76f881b6d47
isxodus <isxodus@gmail.com>
2022-05-02 14:46:41 +0300

    hw9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,29 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        // setTimerId
+        //TODO почему в комменте setTimerId? Я как понимаю, надо просто прервать предыдущий таймер?
+        //У нас же нет никаких переключений между id, чтобы можно было в любой момент остановить любой таймер
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
-    const onMouseEnter = () => {
-        // show
-    }
-    const onMouseLeave = () => {
-        // close
-    }
+    const onMouseEnter = () => setShow(true)
+    const onMouseLeave = () => setShow(false)
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toTimeString().substring(0, 8)// fix with date
+    const stringDate = date.toLocaleDateString("RU-ru")// fix with date
 
     return (
         <div>
