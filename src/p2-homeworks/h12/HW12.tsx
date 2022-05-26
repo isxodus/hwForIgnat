9739025f54a55a767e4c05e2f2ee8b8c76725feb
isxodus <isxodus@gmail.com>
2022-05-26 22:39:31 +0300

    hw12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import s from "./HW12.module.css";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
+import {useDispatch, useSelector} from "react-redux";
+import {changeThemeC} from "./bll/themeReducer";
+import {AppStoreType} from "../h10/bll/store";
 
 const themes = ['dark', 'red', 'some'];
+//создал таким образом тип
+export type themeName = typeof themes[number];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-
+    //const theme = 'some';
+    // useSelector
     // useDispatch, onChangeCallback
+    const theme = useSelector<AppStoreType, themeName>(state => state.theme.themeName)
+    const dispatch = useDispatch()
+    const changeTheme = (e: ChangeEvent<HTMLSelectElement>) => {
+        dispatch(changeThemeC(e.currentTarget.value))
+    }
 
     return (
         <div className={s[theme]}>
@@ -17,6 +28,7 @@ function HW12() {
 
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
+            <SuperSelect options={themes} onChange={changeTheme}/>
 
             <hr/>
         </div>
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
-const initState = {
+import {themeName} from "../HW12";
 
+type SetTheme = {
+    type: "SET-THEME"
+    themeName: themeName
+}
+type ActionType = SetTheme
+
+type ThemeStateType = {
+    themeName: themeName
+}
+
+const initState: ThemeStateType = {
+    themeName: 'dark'
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: ThemeStateType = initState, action: ActionType): ThemeStateType => { // fix any
     switch (action.type) {
-        case "": {
+        case  "SET-THEME":
+            return {themeName: action.themeName};
+        default:
             return state;
-        }
-        default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export const changeThemeC = (themeName: themeName): ActionType => {
+    return {type: "SET-THEME", themeName: themeName}
+}; // fix any
